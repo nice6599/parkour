a546c9cca2566ab3788da45bd81783f69fdf335c
nice6599 <ncs72mc@gmail.com>
2020-05-22 15:52:19 -0400

    basic implimentation of ParkourRun variables

--- src/main/java/com/hackclub/hackcraft/parkour/objects/ParkourRun.java
+++ src/main/java/com/hackclub/hackcraft/parkour/objects/ParkourRun.java
@@ -1,8 +1,21 @@
 package com.hackclub.hackcraft.parkour.objects;
 
+import com.hackclub.hackcraft.parkour.ParkourPlugin;
 import org.bukkit.entity.Player;
 
 public class ParkourRun {
-    Player player;
+
+    private ParkourPlugin plugin;
+    private Player player;
+
+    private ParkourMap currentMap = null;
+    private String lastCheckpoint = null;
+    private long currentTime = 0;
+
+    public ParkourRun(ParkourPlugin plugin, Player player) {
+        this.plugin = plugin;
+        this.player = player;
+    }
+
 
 }
