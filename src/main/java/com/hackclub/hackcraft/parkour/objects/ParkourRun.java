f695c3b135b061892fd0f49e07d5aebacf1777e1
nice6599 <ncs72mc@gmail.com>
2020-05-24 12:03:30 -0400

    add class methods

--- src/main/java/com/hackclub/hackcraft/parkour/objects/ParkourRun.java
+++ src/main/java/com/hackclub/hackcraft/parkour/objects/ParkourRun.java
@@ -1,6 +1,7 @@
 package com.hackclub.hackcraft.parkour.objects;
 
 import com.hackclub.hackcraft.parkour.ParkourPlugin;
+
 import org.bukkit.entity.Player;
 
 public class ParkourRun {
@@ -8,14 +9,34 @@ public class ParkourRun {
     private ParkourPlugin plugin;
     private Player player;
 
-    private ParkourMap currentMap = null;
+    private ParkourMap map = null;
     private String lastCheckpoint = null;
-    private long currentTime = 0;
+    private long startTime = 0;
 
-    public ParkourRun(ParkourPlugin plugin, Player player) {
+    public ParkourRun(ParkourPlugin plugin, Player player, ParkourMap map) {
         this.plugin = plugin;
         this.player = player;
+        this.map = map;
     }
 
+    public Long getStartTime() {
+        return this.startTime;
+    }
+
+    public void setStartTime(Long time) {
+        this.startTime = time;
+    }
+
+    public String getLastCheckpoint() {
+        return this.lastCheckpoint;
+    }
+
+    public void setLastCheckpoint(String newCheckpoint) {
+        this.lastCheckpoint = newCheckpoint;
+    }
+
+    public ParkourMap getMap() {
+        return this.map;
+    }
 
 }
