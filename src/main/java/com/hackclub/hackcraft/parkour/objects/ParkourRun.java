b5ad1304fb441fdfca2812293e04005d217e6673
nice6599 <ncs72mc@gmail.com>
2020-05-26 11:37:34 -0400

    add getTime() method and calculate time

--- src/main/java/com/hackclub/hackcraft/parkour/objects/ParkourRun.java
+++ src/main/java/com/hackclub/hackcraft/parkour/objects/ParkourRun.java
@@ -1,7 +1,6 @@
 package com.hackclub.hackcraft.parkour.objects;
 
 import com.hackclub.hackcraft.parkour.ParkourPlugin;
-
 import org.bukkit.entity.Player;
 
 public class ParkourRun {
@@ -12,6 +11,8 @@ public class ParkourRun {
     private ParkourMap map = null;
     private String lastCheckpoint = null;
     private long startTime = 0;
+    private long endTime = 0;
+    private long time = 0;
 
     public ParkourRun(ParkourPlugin plugin, Player player, ParkourMap map) {
         this.plugin = plugin;
@@ -23,20 +24,32 @@ public class ParkourRun {
         return this.startTime;
     }
 
-    public void setStartTime(Long time) {
-        this.startTime = time;
+    public Long getEndTime() {
+        return this.endTime;
+    }
+
+    public Long getTime() {
+        return this.time;
     }
 
     public String getLastCheckpoint() {
         return this.lastCheckpoint;
     }
 
-    public void setLastCheckpoint(String newCheckpoint) {
-        this.lastCheckpoint = newCheckpoint;
-    }
-
     public ParkourMap getMap() {
         return this.map;
     }
 
+    public void setStartTime(Long time) {
+        this.startTime = time;
+    }
+
+    public void setEndTime(Long time) {
+        this.endTime = time;
+        this.time = (this.endTime - this.startTime);
+    }
+
+    public void setLastCheckpoint(String newCheckpoint) {
+        this.lastCheckpoint = newCheckpoint;
+    }
 }
