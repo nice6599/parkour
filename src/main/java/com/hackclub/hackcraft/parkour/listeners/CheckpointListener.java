681ba9c868a813b02e5e3323000d47355ae9ccf3
nice6599 <ncs72mc@gmail.com>
2020-05-31 23:09:32 -0400

    add data elements to checkpoint listener

--- src/main/java/com/hackclub/hackcraft/parkour/listeners/CheckpointListener.java
+++ src/main/java/com/hackclub/hackcraft/parkour/listeners/CheckpointListener.java
@@ -16,6 +16,9 @@ public class CheckpointListener implements Listener {
 
     @EventHandler
     public void onCheckpoint(CheckpointEvent e) {
-        e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 1.0F, 8.0F);
+        e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 1.0F,
+                8.0F);
+        pl.getDataManager().getData(e.getPlayer()).getCurrentRun()
+                .setLastCheckpoint(e.getCheckpointIndex());
     }
 }
