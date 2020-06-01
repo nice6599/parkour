d8d0b669039a8f2deb2e54b351317dde60fa573f
nice6599 <ncs72mc@gmail.com>
2020-05-31 23:05:29 -0400

    add listeners to parkourplugin.java

--- src/main/java/com/hackclub/hackcraft/parkour/ParkourPlugin.java
+++ src/main/java/com/hackclub/hackcraft/parkour/ParkourPlugin.java
@@ -3,13 +3,14 @@ package com.hackclub.hackcraft.parkour;
 import com.hackclub.hackcraft.parkour.commands.ParkourAdminCommand;
 import com.hackclub.hackcraft.parkour.listeners.CheckpointListener;
 import com.hackclub.hackcraft.parkour.listeners.ComputerMapListener;
+import com.hackclub.hackcraft.parkour.listeners.ParkourEndListener;
+import com.hackclub.hackcraft.parkour.listeners.ParkourStartListener;
 import com.hackclub.hackcraft.parkour.listeners.PlayerMoveListener;
 import com.hackclub.hackcraft.parkour.objects.ParkourMap;
 import com.hackclub.hackcraft.parkour.utils.ParkourUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public class ParkourPlugin extends JavaPlugin {
 
@@ -32,6 +33,8 @@ public class ParkourPlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new ComputerMapListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerMoveListener(this), this);
         getServer().getPluginManager().registerEvents(new CheckpointListener(this), this);
+        getServer().getPluginManager().registerEvents(new ParkourStartListener(this), this);
+        getServer().getPluginManager().registerEvents(new ParkourEndListener(this), this);
 
 
 
